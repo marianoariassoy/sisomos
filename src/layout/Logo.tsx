097f53457f0409a44e5472aa51cfeaa4d726779d
marianoariassoy <hola@marianoarias.soy>
2024-03-21 11:29:18 -0300

    finish

--- src/layout/Logo.tsx
+++ src/layout/Logo.tsx
@@ -1,5 +1,3 @@
-import Logo from '../assets/logo.svg'
-
 const LogoModule = () => {
   return (
     <div className='absolute top-0'>
@@ -9,7 +7,7 @@ const LogoModule = () => {
       >
         <div className='bg-secondary rounded-full w-40 h-40 flex items-center transition-all hover:rotate-6 hover:drop-shadow-md'>
           <img
-            src={Logo}
+            src='/assets/logo.svg'
             alt='Logo'
             className='h-full mt-1 -ml-2'
           />
